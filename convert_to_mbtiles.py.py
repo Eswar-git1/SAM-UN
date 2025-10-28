7339cdfd8bc7b148388b3044792c9c676c093fc9
Eswar-git1 <eswar.git1@example.com>
2025-10-28 10:23:22 +0530

    Keep only app.py - remove all other Python files from repository

--- convert_to_mbtiles.py.py
+++
@@ -1,9 +0,0 @@
-from mbutil import encode
-
-# Input directory and output MBTiles path
-input_dir = r"E:\Project Folder\J&K_QGIS_App\Mapnik"
-output_file = r"E:\Project Folder\J&K_QGIS_App\Mapnik.mbtiles"
-
-# Convert tiles to MBTiles
-encode(input_dir, output_file)
-print(f"Successfully converted {input_dir} to {output_file}")
